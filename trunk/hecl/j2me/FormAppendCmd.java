7c78fb543248c958616fd1b666e9c7f17236416d
davidw <davidw@462031f5-d00f-0410-a15c-b334bd294bc5>
2004-11-20 13:39:51 +0000

    * j2me/ : Added various other bits and pieces of the Hecl.jar example   for j2me devices, namely Hecl.jar, Hecl.jad, and a small README.
    * j2me/FormAppendCmd.java: Added this command, which is just used by
      the MIDHecl example.
    
    * j2me/MIDHecl.java: Added midlet example.

---
+++ trunk/hecl/j2me/FormAppendCmd.java
@@ -0,0 +1,43 @@
+/* Copyright 2004 David N. Welton
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+   	http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+import com.dedasys.hecl.Command;
+import com.dedasys.hecl.Thing;
+import com.dedasys.hecl.HeclException;
+import com.dedasys.hecl.Interp;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+
+
+/**
+ * <code>FormAppendCmd</code> appends a thing as a string to a form.
+ * Not a real command, just used to test output in the Hecl example
+ * MIDlet.
+ *
+ * @author <a href="mailto:davidw@dedasys.com">David N. Welton</a>
+ * @version 1.0
+ */
+
+class FormAppendCmd implements Command {
+
+    public Form mainForm;
+
+    public void cmdCode(Interp interp, Thing[] argv)
+	throws HeclException {
+	String str = argv[1].toString();
+	mainForm.append(str);
+    }
+}
