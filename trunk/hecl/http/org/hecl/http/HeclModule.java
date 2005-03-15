8535b9b9fb3bcf59927672c42013feca6f637877
davidw <davidw@462031f5-d00f-0410-a15c-b334bd294bc5>
2005-03-15 19:01:28 +0000

    Fixed files with DOS line-endings.

--- trunk/hecl/http/org/hecl/http/HeclModule.java
+++ trunk/hecl/http/org/hecl/http/HeclModule.java
@@ -14,17 +14,22 @@ import org.hecl.Thing;
 
 /**
  * @author zoro
- * 
+ *
  * TODO To change the template for this generated type comment go to Window -
  * Preferences - Java - Code Style - Code Templates
  */
 public class HeclModule implements org.hecl.HeclModule, ResHandle {
+
     public int getPriority() {
         return 0;
     }
+
     public Thing getRes(String resourcename) throws HeclException {
-        return new Thing(new StringThing(HttpCommand.executeQuery(resourcename, null, null)));
+        return new Thing(
+	    new StringThing(
+		HttpCommand.executeQuery(resourcename, null, null)));
     }
+
     public boolean handleRes(String resourcename) {
         System.out.println("HANDLE - "+resourcename);
         if (resourcename.length() < 8)
@@ -35,10 +40,12 @@ public class HeclModule implements org.hecl.HeclModule, ResHandle {
         else
             return false;
     }
+
     public void loadModule(Interp interp) throws HeclException {
         interp.addResourceGetter(this);
         interp.addCommand("http", new HttpCommand());
     }
+
     public void unloadModule(Interp interp) throws HeclException {
         interp.removeResourceGetter(this);
         interp.removeCommand("http");
