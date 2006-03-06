4b4eec54f21b7a133c20e16e819d831a43511f4b
davidw <davidw@462031f5-d00f-0410-a15c-b334bd294bc5>
2006-03-06 08:23:10 +0000

    * jarhack/jarhack/JarHack.java: Improved command line functionality -   replaced String.split call with our own function so as to be compatible   with older versions of GCJ that I still need to use.

--- trunk/hecl/jarhack/jarhack/JarHack.java
+++ trunk/hecl/jarhack/jarhack/JarHack.java
@@ -25,6 +25,7 @@ import java.io.InputStream;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.Vector;
 
 import java.util.jar.*;
 
@@ -71,9 +72,11 @@ public class JarHack {
 	    if (keyname.equals("MIDlet-Name")) {
 		attrs.putValue(keyname, newname);
 	    } else if (keyname.equals("MIDlet-1")) {
-		/* FIXME  */
 		String valuestr = value.toString();
-		String properties[] = (valuestr.split(", "));
+		/* FIXME - the stringsplit method is used for older
+		 * versions of GCJ.  Once newer versions are common,
+		 * it can go away.  Or not - it works just fine. */
+		String properties[] = stringsplit(valuestr, ", ");
 		attrs.putValue(keyname, newname + ", " + properties[1] + ", " + properties[2]);
 	    } else if (keyname.equals("MIDlet-Jar-URL")) {
 		attrs.put(key, newname + ".jar");
@@ -117,15 +120,29 @@ public class JarHack {
     }
 
     /**
-     * The <code>createJadForJar</code> method creates a new .jad file
-     * that matches the .jar file passed to it.  appname is the name
-     * of the new application.
+     * The <code>createJadForJar</code> method calls createJadForJar
+     * with a url created from the application's name.
      *
      * @param jarfile a <code>String</code> value
      * @param appname a <code>String</code> value
      * @exception IOException if an error occurs
      */
     public static void createJadForJar(String jarfile, String appname) throws IOException {
+	String url = appname + ".jar";
+	createJadForJar(jarfile, appname, url);
+    }
+
+    /**
+     * The <code>createJadForJar</code> method creates a new .jad file
+     * that matches the .jar file passed to it.  appname is the name
+     * of the new application.
+     *
+     * @param jarfile a <code>String</code> value
+     * @param appname a <code>String</code> value
+     * @param url a <code>String</code> value
+     * @exception IOException if an error occurs
+     */
+    public static void createJadForJar(String jarfile, String appname, String url) throws IOException {
 	File jf = new File(jarfile);
 	String parent = jf.getParent();
 	File jadfile = new File(parent + File.separatorChar + appname + ".jad");
@@ -133,7 +150,7 @@ public class JarHack {
 	of.write("MIDlet-1: "+appname+", Hecl.png, Hecl" + "\n" +
 		 "MIDlet-Info-URL: http://www.hecl.org" + "\n" +
 		 "MIDlet-Jar-Size: " + jf.length() + "\n" +
-		 "MIDlet-Jar-URL: "+appname+".jar" + "\n" +
+		 "MIDlet-Jar-URL: " + url + "\n" +
 		 "MIDlet-Name: " + appname + "\n" +
 		 "MIDlet-Vendor: DedaSys" + "\n" +
 		 "MIDlet-Version: 1.0" + "\n" +
@@ -142,15 +159,76 @@ public class JarHack {
 	of.close();
     }
 
-    public static void main(String[] args) {
 
-	String newfn = "/tmp/" + args[1] + ".jar";
+    /**
+     *  <code>main</code> implements a command line version of JarHack
+     *  that takes five arguments: 1) The original 'template' .jar
+     *  file to use.  2) The destination directory where you want to
+     *  create your new application.  3) The name of your new
+     *  application as it will appear in the MIDlet.  4) The name of
+     *  the Hecl script you want to use instead of the script.hcl that
+     *  is included in the 'source' .jar file.  5) The URL where your
+     *  jar file will reside.
+     *
+     * @param args a <code>String[]</code> value
+     */
+    public static void main(String[] args) {
+	if (args.length < 4 || args.length > 5) {
+	    System.err.println(
+		"Usage: JarHack original.jar destination_dir ApplicationName yourscript.hcl ?url?");
+	    System.exit(1);
+	}
+
+	String sourcefile = args[0];
+	String destdir = args[1];
+	String appname = args[2];
+	String scriptfile = args[3];
+	String url;
+	if (args.length == 5) {
+	    url = args[4];
+	} else {
+	    url = appname + ".jar";
+	}
+
+	if (!(new File(destdir)).isDirectory()) {
+	    System.err.println("Not a valid directory: " + destdir);
+	    System.exit(1);
+	}
+
+	String newfn =  destdir + appname + ".jar";
 
 	try {
-	    FileInputStream infile = new FileInputStream(args[0]);
-	    substHecl(infile, newfn, args[1], args[2]);
+	    FileInputStream infile = new FileInputStream(sourcefile);
+	    substHecl(infile, newfn, appname, scriptfile);
+	    createJadForJar(newfn, appname, url);
 	} catch (Exception e) {
 	    System.err.println("Error: " + e);
+	    e.printStackTrace();
 	}
+    }
+
+    /**
+     * The <code>stringsplit</code> method is here because older
+     * versions of GCJ don't seem to handle String.split very well.
+     *
+     * @param in a <code>String</code> value
+     * @param split a <code>String</code> value
+     * @return a <code>String[]</code> value
+     */
+    private static String[] stringsplit(String in, String split) {
+	Vector ret = new Vector();
+	int fromindex = 0;
+	int idx = 0;
+	int splitlen = split.length();
+	while (true) {
+	    idx = in.indexOf(split, fromindex);
+	    if (idx < 0) break;
+	    ret.addElement(in.substring(fromindex, idx));
+	    fromindex = idx + splitlen;
+	}
+	ret.addElement(in.substring(fromindex));
+	String[] retstr = new String[ret.size()];
+	ret.copyInto(retstr);
+	return retstr;
     }
 }
