9eacf7bff8691e6a2128908af097e2cae6820f96
davidw <davidw@462031f5-d00f-0410-a15c-b334bd294bc5>
2008-07-12 08:17:24 +0000

    * core.xml: Build JarHack with commons cli in order to be able to use   real command line options.
    * jarhack/org/hecl/jarhack/JarHack.java: Moved JarHack into an
      org.hecl.jarhack namespace

--- trunk/hecl/jarhack/org/hecl/jarhack/JarHack.java
+++ trunk/hecl/jarhack/org/hecl/jarhack/JarHack.java
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package jarhack;
+package org.hecl.jarhack;
 
 import java.io.IOException;
 
