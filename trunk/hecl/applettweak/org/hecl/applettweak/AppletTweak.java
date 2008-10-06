5fc6c57dff797158b3f81afe71435180538ff8c1
davidw <davidw@462031f5-d00f-0410-a15c-b334bd294bc5>
2008-10-06 20:31:12 +0000

    * applettweak/org/hecl/applettweak/AppletTweak.java: Wrote a bit of   code to perform the necessary bytecode hacks on the Hecl jars in   order to make the work as applets in microemulator.

---
+++ trunk/hecl/applettweak/org/hecl/applettweak/AppletTweak.java
@@ -0,0 +1,40 @@
+/* Copyright 20008 DedaSys LLC - http://www.dedasys.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package org.hecl.applettweak;
+
+import org.microemu.app.util.AppletProducer;
+import java.io.File;
+
+public class AppletTweak {
+
+    public static void main(String[] args) {
+	if (args.length < 2) {
+	    usage();
+	    return;
+	}
+	try {
+	    File output = new File(args[1]);
+	    AppletProducer.createMidlet(args[0], output);
+	} catch (Exception ex) {
+	    System.err.println("Error: " + ex.toString());
+	}
+    }
+
+    private static void usage() {
+	System.err.println("Usage: AppletTweak file:///url/to/infile.jar outfile");
+    }
+
+}
