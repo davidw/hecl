a08e7d3f9915b4c7293806c471e9928c54596444
David N. Welton <davidw@dedasys.com>
2009-11-05 15:40:19 +0100

    Eliminated commented code, trailing whitespace, updated copyright.

--- core/org/hecl/Proc.java
+++ core/org/hecl/Proc.java
@@ -1,4 +1,4 @@
-/* Copyright 2004-2006 David N. Welton
+/* Copyright 2004-2009 David N. Welton, Wolfgang Kechel
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -71,7 +71,7 @@ class Proc implements Command {
 	    // vector for varargs
 	    vargvals = new Vector();
 	}
-	
+
 	int i = argv.length - 1;
 	if (i < argc)
 	    throw new HeclException("proc " + argv[0]
@@ -88,14 +88,14 @@ class Proc implements Command {
 		//System.err.println("set "+this.argnames[i]+"="+argv[i + 1].toString());
 		interp.setVar(this.argnames[i],argv[i + 1]);
 	    }
-	    
+
 	    /* Hoover up anything left over as varargs. */
 	    for (; i < argv.length - 1; i++ ) {
 		vargvals.addElement(argv[i + 1]);
 	    }
 	    if(vargvals != null)
 		interp.setVar(VARARGVARNAME, ListThing.create(vargvals));
-	    
+
 	    /* We actually run the code here. */
 	    Thing res = null;
 	    try {
@@ -125,97 +125,3 @@ class Proc implements Command {
 	return code;
     }
 }
-
-
-//class Proc implements Command {
-//    private Thing vars;
-//    private Thing code;
-//
-//    /**
-//     * Creates a new <code>Proc</code> instance, with the variable names in
-//     * cmdvars, and the actual code in cmdcode.
-//     *
-//     * @param cmdvars a <code>Thing</code> value
-//     * @param cmdcode a <code>Thing</code> value
-//     */
-//    public Proc(Thing cmdvars, Thing cmdcode) {
-//        vars = cmdvars;
-//        code = cmdcode;
-//    }
-//
-//    private static final String varargvarname = "args";
-//
-//    public Thing cmdCode(Interp interp, Thing[] argv) throws HeclException {
-//        Vector varnames = ListThing.get(vars);
-//        int i = 0;
-//	Vector vargvals = null;
-//
-//        /* Push a new frame onto the stack. */
-//        interp.stackIncr();
-//
-//        /* Create the argument variables. */
-//	int argc = varnames.size();
-//
-//	/* If the last element of the variable names is called 'args',
-//	 * then it is a list that can accumulate any 'extra' args
-//	 * passed in to the proc. */
-//	if (argc > 0 && ((Thing) varnames.elementAt(argc - 1)).toString().equals(varargvarname)) {
-//	    vargvals = new Vector();
-//	    argc --;
-//	}
-//	HeclException he = null;
-//	if (argv.length - 1 < argc) {
-//	    he = new HeclException("proc " + argv[0]
-//				    + " doesn't have enough arguments");
-//	} else if (argv.length - 1 > argc && vargvals == null) {
-//            he = new HeclException("proc " + argv[0]
-//                    + " has too many arguments");
-//	}
-//	if (he != null) {
-//	    interp.stackDecr();
-//	    throw he;
-//	}
-//
-//	/* Set the variables from argv.  Add one to argv, because
-//	 * argv0 is the name of the proc itself. */
-//        for (i = 0; i < argc; i++) {
-//            interp.setVar(((Thing) varnames.elementAt(i)).toString(),
-//			  argv[i + 1]);
-//        }
-//
-//	/* Hoover up anything left over as varargs. */
-//	for (; i < argv.length - 1; i++ ) {
-//	    vargvals.addElement(argv[i + 1]);
-//	}
-//	if(vargvals != null)
-//	    interp.setVar(varargvarname, ListThing.create(vargvals));
-//
-//	/* We actually run the code here. */
-//	Thing res = null;
-//        try {
-//            res = interp.eval(code);
-//        } catch (HeclException e) {
-//            if (e.code != HeclException.RETURN) {
-//		interp.stackDecr();
-//                throw e;
-//            } else {
-//		res = e.value;
-//	    }
-//        }
-//
-//        /* We're done, pop the stack. */
-//        interp.stackDecr();
-//        //Hashtable ht = interp.stackDecr();
-//	//ht = null;
-//	return res;
-//    }
-//
-//    /**
-//     * <code>getCode</code> returns the proc's code.
-//     *
-//     * @return a <code>Thing</code> value
-//     */
-//    public Thing getCode() {
-//	return code;
-//    }
-//}
