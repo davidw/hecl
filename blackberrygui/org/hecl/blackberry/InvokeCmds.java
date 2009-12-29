c449158a2a4be546899427574794a45f5f9afdcf
David N. Welton <davidw@dedasys.com>
2009-12-29 16:07:54 +0100

    Use jde.version to exclude older API's.
    We use the jde.version variable to keep track of whate JDE version we
    are compiling with, and, as a consequence, exclude features not
    available on older JDE's.

--- blackberrygui/org/hecl/blackberry/InvokeCmds.java
+++ blackberrygui/org/hecl/blackberry/InvokeCmds.java
@@ -24,7 +24,9 @@ import net.rim.blackberry.api.invoke.AddressBookArguments; /* FIXME - TODO */
 import net.rim.blackberry.api.invoke.ApplicationArguments;
 import net.rim.blackberry.api.invoke.CalculatorArguments;
 import net.rim.blackberry.api.invoke.CalendarArguments; /* FIXME - TODO */
+//#if jde.version > 4.3
 import net.rim.blackberry.api.invoke.CameraArguments;
+//#endif
 import net.rim.blackberry.api.invoke.PhoneArguments;
 
 import java.util.Hashtable;
@@ -56,6 +58,7 @@ public class InvokeCmds extends Operator {
 	    }
 
 
+//#if jde.version > 4.3
 	    case CAMERA: {
 		Invoke.invokeApplication(Invoke.APP_TYPE_CAMERA,
 					 new CameraArguments(CameraArguments.ARG_CAMERA_APP));
@@ -67,7 +70,7 @@ public class InvokeCmds extends Operator {
 					 new CameraArguments(CameraArguments.ARG_VIDEO_RECORDER));
 		return Thing.emptyThing();
 	    }
-
+//#endif
 
 	  default:
 	    throw new HeclException("Unknown browser command '"
