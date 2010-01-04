6aec4dae02bace12e6ca8fe4cf0474d2ee720195
David N. Welton <davidw@catlow.(none)>
2010-01-04 11:47:51 +0100

    Fiddled with JDE version numbers for CameraArguments - video is only available on recent (4.7) JDE's.

--- blackberrygui/org/hecl/blackberry/InvokeCmds.java
+++ blackberrygui/org/hecl/blackberry/InvokeCmds.java
@@ -24,7 +24,7 @@ import net.rim.blackberry.api.invoke.AddressBookArguments; /* FIXME - TODO */
 import net.rim.blackberry.api.invoke.ApplicationArguments;
 import net.rim.blackberry.api.invoke.CalculatorArguments;
 import net.rim.blackberry.api.invoke.CalendarArguments; /* FIXME - TODO */
-//#if jdeversion > 4.3
+//#if jdeversion > 4.2
 import net.rim.blackberry.api.invoke.CameraArguments;
 //#endif
 import net.rim.blackberry.api.invoke.PhoneArguments;
@@ -58,13 +58,13 @@ public class InvokeCmds extends Operator {
 	    }
 
 
-//#if jdeversion > 4.3
 	    case CAMERA: {
 		Invoke.invokeApplication(Invoke.APP_TYPE_CAMERA,
-					 new CameraArguments(CameraArguments.ARG_CAMERA_APP));
+					 new CameraArguments());
 		return Thing.emptyThing();
 	    }
 
+//#if jdeversion >= 4.7
 	    case VIDEO: {
 		Invoke.invokeApplication(Invoke.APP_TYPE_CAMERA,
 					 new CameraArguments(CameraArguments.ARG_VIDEO_RECORDER));
@@ -99,8 +99,8 @@ public class InvokeCmds extends Operator {
 	try {
 	    cmdtable.put("invoke.call", new InvokeCmds(CALL,1,1));
 	    cmdtable.put("invoke.calculator", new InvokeCmds(CALCULATOR,0,0));
-//#if jdeversion > 4.3
 	    cmdtable.put("invoke.camera", new InvokeCmds(CAMERA,0,0));
+//#if jdeversion >= 4.7
 	    cmdtable.put("invoke.video", new InvokeCmds(VIDEO,0,0));
 //#endif
 	} catch (Exception e) {
