de1d7e13d41d68324071efbc1b36f757b45d11cb
David N. Welton <davidw@catlow.(none)>
2010-01-30 12:09:50 +0200

    Added copyright, javadoc.

--- core/org/hecl/AnonProc.java
+++ core/org/hecl/AnonProc.java
@@ -1,5 +1,27 @@
+/* Copyright 2010 David N. Welton, DedaSys LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
 package org.hecl;
 
+/**
+ * The <code>AnonProc</code> class implements anonymous procedures,
+ * created like so: set aproc [proc {x} { puts $x }]
+ *
+ * @author <a href="mailto:davidw@dedasys.com">David N. Welton</a>
+ * @version 1.0
+ */
 public class AnonProc implements ClassCommand {
 
     public Thing method(Interp interp, ClassCommandInfo context, Thing[] argv)
@@ -14,5 +36,4 @@ public class AnonProc implements ClassCommand {
 	Proc proc = (Proc)((ObjectThing)rt).get();
 	return proc.cmdCode(interp, newargv);
     }
-
 }
